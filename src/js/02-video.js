8b4af32260a3de3cbe664f43cfa82d4f90e77cea
IhorPinchuk <pinchukkk@ukr.net>
2023-03-11 12:04:50 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(STORAGE_KEY, e.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .catch(function (error) {
+    console.error(error);
+  });
+
+
 
